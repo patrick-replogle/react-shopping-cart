f969c45362bab3fa0b0e43fe264b649e4b7bf6d6
patrick-replogle <patrickr1138@gmail.com>
2019-11-09 15:12:21 -0800

    Additem and removeItem functions now working. Also, added localstorage, but haven't gotten it to work yet.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+import { createDecipher } from "crypto";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  const removeItem = id => {
+    const newArray = cart.filter(product => {
+      return product.id !== id;
+    });
+    setCart(newArray);
+  };
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+  return (
+    <div className="App">
+      <Navigation cart={cart} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+      {/* Routes */}
+      <Route
+        exact
+        path="/"
+        render={() => <Products products={products} addItem={addItem} />}
+      />
+
+      <Route
+        path="/cart"
+        render={() => <ShoppingCart cart={cart} removeItem={removeItem} />}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 
 // Components
-import Product from './Product';
+import Product from "./Product";
 
 const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+  return (
+    <div className="products-container">
+      {props.products.map(product => (
+        <Product
+          key={product.id}
+          product={product}
+          addItem={props.addItem}
+          removeItem={props.removeItem}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
